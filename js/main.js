ca38395a1bc33f73e0d4e474966bb6ee18f3442f
Ruslan <ruslan.leonoff2014@yandex.ru>
2023-12-22 16:00:40 +0300

    function onclick

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ var counter = 0;
 function onclickButton (el) {
     counter++;
     el.innerHTML = "Вы нажали на кнопку: " + counter;
-    
-    console.log()
+    el.style.background = "red";
+    el.style.color = "blue";
+    console.log();
 }
