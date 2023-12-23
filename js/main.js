4e0f43c03d4723af991323170d06ffe65e2f596c
Ruslan <ruslan.leonoff2014@yandex.ru>
2023-12-23 14:09:50 +0300

    style var

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 var text = document.getElementById('text') ;
+text.title = "my text";
+console.log(text.title);
+
+text.style.color = "blue";
+text.style.backgroundColor = "red";
+
+text.innerHTML = "my<br>string";
