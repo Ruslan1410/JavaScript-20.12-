6513fe39b3c80a0ed4c71ca277ffe4e2f82bd15d
Ruslan <ruslan.leonoff2014@yandex.ru>
2023-12-23 13:54:12 +0300

    getElement

--- js/main.js
+++ js/main.js
@@ -1,9 +1,1 @@
-var counter = 0;
-
-function onclickButton (el) {
-    counter++;
-    el.innerHTML = "Вы нажали на кнопку: " + counter;
-    el.style.background = "red";
-    el.style.color = "blue";
-    console.log();
-}
+var text = document.getElementById('text') ;
